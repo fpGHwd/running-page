1880fdfb946fd6b3f648ac7f0bd46a30543a02bc
Wang Ding <ggwdwhu@gmail.com>
2023-07-17 00:41:53 +0800

    update data

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,17 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://running.autove.dev',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
+        name: 'Running',
+        url: 'https://running.autove.dev',
+      },
+      {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://blog.autove.dev',
       },
       {
         name: 'About',
