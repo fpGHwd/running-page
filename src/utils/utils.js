cec2e8ea54eb92743becb3e7e9f58e2b0031d783
Wang Ding <ggwdwhu@gmail.com>
2023-08-29 23:56:30 +0800

    use origin info of run type.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -177,27 +177,8 @@ const titleForRun = (run) => {
       return RUN_TITLES.EVENING_RUN_TITLE;
     }
     return RUN_TITLES.NIGHT_RUN_TITLE;
-  }else if(runType == "Ride"){
-    // if (runDistance > 20 && runDistance < 40) {
-    //   return RUN_TITLES.HALF_MARATHON_RUN_TITLE;
-    // }
-    // if (runDistance >= 40) {
-    //   return RUN_TITLES.FULL_MARATHON_RUN_TITLE;
-    // }
-    if (runHour >= 0 && runHour <= 10) {
-      return RUN_TITLES.MORNING_RIDE_TITLE;
-    }
-    if (runHour > 10 && runHour <= 14) {
-      return RUN_TITLES.MIDDAY_RIDE_TITLE;
-    }
-    if (runHour > 14 && runHour <= 18) {
-      return RUN_TITLES.AFTERNOON_RIDE_TITLE;
-    }
-    if (runHour > 18 && runHour <= 21) {
-      return RUN_TITLES.EVENING_RIDE_TITLE;
-    }
-    return RUN_TITLES.NIGHT_RIDE_TITLE;
   }
+  return runType;
 };
 
 const applyToArray = (func, array) => func.apply(Math, array);
